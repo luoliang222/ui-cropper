74abe0c07c2fe5e29c8cd74f506102196bfae1de
cjy37 <rocky.cn@foxmail.com>
2017-02-28 19:18:55 +0800

    fixed result size 200*200 bug

--- source/js/ui-cropper.js
+++ source/js/ui-cropper.js
@@ -32,7 +32,7 @@ angular.module('uiCropper').directive('uiCropper', ['$timeout', 'cropHost', 'cro
              /* if canvas is 500x500 Crop coordinates will be x: 50, y: 50, w: 50, h: 50 */
             /* if canvas is 100x100 crop coordinates will be x: 10, y: 10, w: 10, h: 10 */
             areaMinRelativeSize: '=?',
-            resultImageSize: '=?',
+            resultImageSize: '@',
             resultImageFormat: '@',
             resultImageQuality: '=?',
 
